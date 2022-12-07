0890a760890549c43ca0f9c482aea3d0fb128fcd
ExoCoders <akashdebnathwd@gmail.com>
2022-12-07 14:45:53 +0530

    added level 3 security-hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
+// const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -29,10 +30,8 @@ const userSchema = new mongoose.Schema({
 
 
 // Secret String Instead of Two Keys
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password','email'] });    ------- adding multiple field
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password','email'] });       ------- adding multiple field
 
 
 
@@ -66,7 +65,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if (err) {
@@ -77,9 +76,11 @@ app.post("/register", function(req, res){
     });
 });
 
+
+
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
