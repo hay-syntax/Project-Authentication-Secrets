e7c6804c08666ca4af90cc8f47846080aa38c37c
ExoCoders <akashdebnathwd@gmail.com>
2022-12-09 16:23:45 +0530

    OAuth 2.0 with Google Authentication

--- app.js
+++ app.js
@@ -8,24 +8,14 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
-
-
-// ----------------------------------------------------------------------------------------------------------------------
-// Using bcryptjs (Level-4 Security)
-// const bcrypt = require('bcryptjs');
-// const salt = bcrypt.genSaltSync(10); 
-// ----------------------------------------------------------------------------------------------------------------------
-// const md5 = require("md5");
-// ----------------------------------------------------------------------------------------------------------------------
-// const encrypt = require('mongoose-encryption');
-// ----------------------------------------------------------------------------------------------------------------------
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 
 const app = express();
 
-// console.log(process.env.API_KEY);
+
 
 app.set('view engine', 'ejs');
 mongoose.set('strictQuery', true);
@@ -48,18 +38,13 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB");
 // Mongoose Schema
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-// ----------------------------------------------------------------------------------------------------------------------
-
-// Secret String Instead of Two Keys
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password','email'] });       ------- adding multiple field
-
-// ----------------------------------------------------------------------------------------------------------------------
 
 // Mongoose model
 const User = new mongoose.model("User", userSchema);
@@ -68,8 +53,31 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 /////////////////////////////////////////////////----- Get Method -----////////////////////////////////////////////////////////// 
@@ -80,6 +88,19 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+});
+
+
 
 app.get("/login", function(req, res){
     res.render("login");
